c88ee3399154b99221292fc6c95fa99008d794c8
v <vladlonglake@gmail.com>
2019-09-18 14:02:11 +0300

    v0.1.0

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ExampleScrape() {
+	// Request the HTML page.
+	res, err := http.Get("https://www.avito.ru/moskva/noutbuki?cd=1&q=x1+carbon")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	doc.Find(".layout-internal .l-content .catalog-content .catalog-main .item .item_table-header").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		link, _ := s.Find("a").Attr("href")
+		name := s.Find("h3").Text()
+		price := s.Find(".price").Text()
+		fmt.Printf("%s %s %s\n", name, link, price)
+	})
+}
+
+func main() {
+	ExampleScrape()
+}
